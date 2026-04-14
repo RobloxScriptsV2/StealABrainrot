fa0cfeb30a13e3048464bb36ad836503c9945f69
RobloxScriptsV2 <mernaif3@gmail.com>
2026-04-14 14:50:43 +0800

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://pastefy.app/Qkcb9g07/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/ytyP4wki/raw"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/real-script/MyOwnLoadingScript/refs/heads/main/Script.lua"))()
